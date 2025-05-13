30f13c2dc6c38fad239390df1bde33313c66d247
hak33m84 <hak33m84@gmail.com>
2025-05-13 22:53:52 +0700

    projects updated

--- src/constants/project-data.ts
+++ src/constants/project-data.ts
@@ -14,14 +14,15 @@ export const projectsData: Project[] = [
   {
     imageSrc: project1,
     name: 'Todo List App',
-    description: 'Implementation of todo list with CRUD features.',
+    description:
+      'Implemented a Todo List application featuring complete CRUD functionalities using React JS and Axios, styled with CSS Modules for modular and maintainable design. Integrated infinite scroll and data fetching on viewport using Intersection Observer API to enhance performance and ensure a fast-loading user experience. Configured and consumed RESTful APIs documented with Swagger.',
     href: 'https://to-do-list-ten-umber-64.vercel.app/',
   },
   {
     imageSrc: project2,
     name: 'Company Profile Website',
     description:
-      'Profesional company profile website with responsive design. Using Next.js, Shadcn UI, Framer Motion and Tailwind CSS.',
+      'Built a professional company profile website with a responsive design that works seamlessly on both mobile and desktop. The project was developed using Next.js and styled with Tailwind CSS, combining modern layout techniques like Flexbox, Grid, and clamp() for flexible and clean design. Used Shadcn UI components (such as cards, forms, tabs, accordions, and carousels) to speed up development and maintain consistent UI. Added smooth skew animations with Framer Motion to improve user experience. The codebase was written in both TypeScript and JavaScript for better structure and maintainability.',
     href: 'https://company-profile-rho-silk.vercel.app/',
   },
 ];
